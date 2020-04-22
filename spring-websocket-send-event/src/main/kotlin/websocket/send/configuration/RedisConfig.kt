6120029aaacc1dc4bb07c99f707d7c3d085ec87e
Lincoln Pires <lincoln.ufop@gmail.com>
2020-04-22 09:15:09 -0300

    adjustments redis settings

--- spring-websocket-send-event/src/main/kotlin/websocket/send/configuration/RedisConfig.kt
+++ spring-websocket-send-event/src/main/kotlin/websocket/send/configuration/RedisConfig.kt
@@ -1,9 +1,11 @@
 package websocket.send.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.listener.ChannelTopic
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 
 /**
@@ -12,6 +14,9 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 @Configuration
 class RedisConfig {
 
+    @Value("\${spring.redis.topic}")
+    lateinit var redisTopic: String
+
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
         val redisTemplate: RedisTemplate<String, Any> = RedisTemplate()
@@ -19,4 +24,7 @@ class RedisConfig {
         redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
         return redisTemplate
     }
+
+    @Bean
+    fun topic(): ChannelTopic = ChannelTopic(redisTopic)
 }
--- spring-websocket-send-event/src/main/kotlin/websocket/send/web/PublishRedisController.kt
+++ spring-websocket-send-event/src/main/kotlin/websocket/send/web/PublishRedisController.kt
@@ -2,7 +2,9 @@ package websocket.send.web
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.listener.ChannelTopic
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import websoket.core.domain.InputMessage
@@ -12,16 +14,19 @@ import websoket.core.domain.InputMessage
  */
 @RestController
 @RequestMapping(value = ["send"])
-class PublishRedisController(private val redisTemplate: RedisTemplate<String, Any>){
+class PublishRedisController(private val redisTemplate: RedisTemplate<String, Any>,
+                             private val channelTopic: ChannelTopic){
 
+    @Value("\${spring.redis.topic}")
+    private val eventTopic: String? = null
     private val logger: Logger = LoggerFactory.getLogger(PublishRedisController::class.java)
 
-    @PostMapping(value = ["/event-redis"])
+    @PostMapping(value = ["/publish-event-redis"])
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     fun sendEventToBrokerRedis(@RequestBody inputMessage: InputMessage) {
         if (inputMessage.message.isNotEmpty()) {
             logger.info("SEND_EVENT-SIMULATION-PUBSUB-REDIS")
-            redisTemplate.convertAndSend(inputMessage.customerId, inputMessage)
+            redisTemplate.convertAndSend(channelTopic.topic, inputMessage)
         } else {
             logger.error("ERROR_SENDING_EVENT_TO_BROKER_REDIS")
         }
